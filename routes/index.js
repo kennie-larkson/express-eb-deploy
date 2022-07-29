90d487247dd42714c2edf78dcecb9a55e9b76e86
kennie-larkson <materialkenny.com>
2022-07-29 10:34:47 +0100

    Removed template engine implementation.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Kennie! Elastic Beanstalk App" });
+// });
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Kennie! Elastic Beanstalk App" });
+  res.send("Welcome to Kennie, Elastic Beanstalk App.");
 });
 
 module.exports = router;
