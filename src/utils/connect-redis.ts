8621ac476d2237ea56ad6b6c5b555373d3016db3
kennie-larkson <materialkenny.com>
2022-07-30 00:25:13 +0100

    Removed redisUrl.

--- src/utils/connect-redis.ts
+++ src/utils/connect-redis.ts
@@ -2,9 +2,11 @@ import { createClient } from "redis";
 
 const redisUrl = "redis://localhost:6379";
 
-const redisClient = createClient({
-  url: redisUrl,
-});
+const redisClient =
+  createClient();
+  //     {
+  //   url: redisUrl,
+  // }
 
 const connectRedis = async () => {
   try {
