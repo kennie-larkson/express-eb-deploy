373ecd55d0598dfe6ef5bc76e5b86a61c6689d24
kennie-larkson <materialkenny.com>
2022-08-02 13:22:33 +0100

    changed redis connection origin from localhost to awsebs.

--- config/defaults.ts
+++ config/defaults.ts
@@ -1,6 +1,6 @@
 export default {
   //origin: "http://localhost:3000",
-  origin: "*",
+  origin: "http://ebnodeapp2-env.eba-uqfbkmc4.us-west-2.elasticbeanstalk.com/",
   accessTokenExpiresIn: 15,
   refreshTokenExpiresIn: 60,
   redisCacheExpiresIn: 60,
