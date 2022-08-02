f96cfec8258ed5587ec73d6493f5e0355d63cf6b
kennie-larkson <materialkenny.com>
2022-08-02 13:00:10 +0100

    fixed database connection bug.

--- src/utils/connect-redis.ts
+++ src/utils/connect-redis.ts
@@ -2,11 +2,9 @@ import { createClient } from "redis";
 
 const redisUrl = "redis://localhost:6379";
 
-const redisClient =
-  createClient();
-  //     {
-  //   url: redisUrl,
-  // }
+const redisClient = createClient({
+  url: redisUrl,
+});
 
 const connectRedis = async () => {
   try {
